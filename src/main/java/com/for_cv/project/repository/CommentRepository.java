9247a20ea881d6bb7dd471bd647fbec9ef5d454c
true-false-try <6mateo6ch@gmail.com>
2023-09-04 14:30:35 +0300

    Created CommentRepository with 2 methods

---
+++ src/main/java/com/for_cv/project/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.for_cv.project.repository;
+
+import com.for_cv.project.entity.Comment;
+import com.for_cv.project.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+
+    Comment findByIdAndUserId(Long commentId, Long userId);
+}
