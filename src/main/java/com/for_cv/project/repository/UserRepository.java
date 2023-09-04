980f7a05c6bf21d863b986366800d407ab03c081
true-false-try <6mateo6ch@gmail.com>
2023-09-04 14:20:44 +0300

    Created userRepository with 3 methods

---
+++ src/main/java/com/for_cv/project/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.for_cv.project.repository;
+
+import com.for_cv.project.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+}
