dca8531fb5742abd067ded5fdb85ecb836e76c91
true-false-try <6mateo6ch@gmail.com>
2023-09-04 14:25:46 +0300

    Created PostRepository with 3 methods

---
+++ src/main/java/com/for_cv/project/repository/PostRepository.java
@@ -0,0 +1,19 @@
+package com.for_cv.project.repository;
+
+import com.for_cv.project.entity.Post;
+import com.for_cv.project.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByUserOrderByCreateDateDesc(User user);
+
+    List<Post> findAllByOOrderByCreateDateDesc();
+
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+}
