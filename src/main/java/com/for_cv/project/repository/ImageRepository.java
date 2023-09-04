32fa44decc698f6cc947e9a4fc1d1b05378f81d9
true-false-try <6mateo6ch@gmail.com>
2023-09-04 14:28:02 +0300

    Created ImageRepository with 2 methods

---
+++ src/main/java/com/for_cv/project/repository/ImageRepository.java
@@ -0,0 +1,16 @@
+package com.for_cv.project.repository;
+
+import com.for_cv.project.entity.ImageModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ImageRepository extends JpaRepository<ImageModel, Long> {
+
+    Optional<ImageModel> findByUserId(Long userId);
+
+    Optional<ImageModel> findByPostId(Long postId);
+
+}
