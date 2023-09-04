107b20e3c28a5079bf349a668dfd9d38745c9b3c
true-false-try <6mateo6ch@gmail.com>
2023-09-04 13:16:02 +0300

    Created Comment entity

--- src/main/java/com/for_cv/project/entity/Comment.java
+++ src/main/java/com/for_cv/project/entity/Comment.java
@@ -1,4 +1,31 @@
 package com.for_cv.project.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Post post;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private Long userId;
+    @Column(columnDefinition = "text", nullable = false)
+    private String message;
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    private void ondate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
