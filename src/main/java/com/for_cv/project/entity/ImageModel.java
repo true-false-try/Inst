99856795824090c89b26e9e5b1f0d5b90d400819
true-false-try <6mateo6ch@gmail.com>
2023-09-04 13:31:19 +0300

    Created ImageModel entity

--- src/main/java/com/for_cv/project/entity/ImageModel.java
+++ src/main/java/com/for_cv/project/entity/ImageModel.java
@@ -1,4 +1,25 @@
 package com.for_cv.project.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class ImageModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    // @Lob means a large object in db like as byte[] or characters for images,videos,etc.
+    // @Column(columnDefinition = "LONGBLOB") use to set type our db [MySQL]
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] imageBytes;
+    // @JsonIgnore use it when we want to ignore this field in data response on client
+    @JsonIgnore
+    private Long userId;
+    @JsonIgnore
+    private Long postId;
 }
